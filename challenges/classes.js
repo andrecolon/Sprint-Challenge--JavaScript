2129033ff4e6757da02a6ffe83a8514ab236c0a2
Andre <andrecolon@gmail.com>
2020-02-23 21:00:40 -0800

    Almost there

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,4 +1,29 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
+class CuboidMaker {
+    constructor(atrr) {
+        this.length = atrr.length;
+        this.width = atrr.width;
+        this.height = atrr.height;
+    }
+    volume() {
+        return this.length * this.width * this.height
+    }
+    surfaceArea() {
+        return 2 * this.length * this.width + this.length * this.height + this.width * this.height
+    }
+}
+
+class CubeMaker extends CuboidMaker {
+    constructor(atrr) {
+        super(atrr);
+        this.color = atrr.color;
+    }
+}
+
+const jellyCube = new CubeMaker({ length: 3, width: 5, height: 2, color: "red" })
+
+
+console.log(jellyCube)
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
